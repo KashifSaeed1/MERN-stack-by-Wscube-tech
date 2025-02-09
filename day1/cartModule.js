29d62bbfec5e8f7f0db972245392ba6c2f644e81
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-09 12:56:01 +0500

    Chapter 4 completed

---
+++ day1/cartModule.js
@@ -0,0 +1,19 @@
+let addToCart = () => {
+    return "Add to cart"
+}
+
+
+
+let changeQuantity = () => { 
+        return 5
+}
+let name = "Ws"
+
+
+// default export
+// module.exports = addToCart
+
+// named export
+module.exports = {addToCart, changeQuantity, name}
+ 
+
---
+++ day1/index.js
@@ -0,0 +1,8 @@
+const {addToCart, changeQuantity, name} = require("./cartModule")
+
+ console.log('hello')
+
+ console.log('addToCart' , addToCart())
+console.log('changeQuantity',   changeQuantity())
+console.log("name", name)
+ 
